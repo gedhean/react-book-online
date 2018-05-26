0587ad6f9f714f6e2305b49ae95c44e0f5edfbee
Gêdhean Alves Vieira <gedhean1@gmail.com>
2018-05-26 19:27:58 -0300

    just testing react-pdf

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world! o/
         <Book />
       </div>
     );
--- src/componets/Book.js
+++ src/componets/Book.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
-import { Document, Page } from 'react-pdf';
-import  file from '../test.pdf';
+import { Page } from 'react-pdf';
+import { Document, Outline } from 'react-pdf/dist/entry.webpack'; // Using pdf.js workers for performance
+import file from '../assets/test.pdf';
 
-const Book = (props) => {
+class Book extends Component {
+  state = {
+    page: 1
+  };
+
+  render() {
     return (
-        <div>
-            <Document fille={'../test.pdf'}> 
-                <Page pagaNumber={23} />
-            </Document>
-        </div>
+      <div>
+        <button onClick={() => this.setState(ps => ({ page: ps.page > 2 ? ps.page - 1 : 0 }))}>
+          prev page
+        </button>
+
+        <button onClick={() => this.setState(ps => ({ page: ps.page + 1 }))}>
+          next page
+        </button>
+        <Document file={file} loading={'Please, wait sir!'}>
+          <Page
+            pageNumber={this.state.page}
+            //customTextRenderer={({ str, itemIndex }) => {
+            //return <mark>{str + ' |$'}</mark>;
+            //}}
+            scale={1.6}
+            loading={null}
+          />
+          {/* <Page pageNumber={124} scale={1.5} />
+          <Page pageNumber={125} width={900} />
+          <Page pageNumber={126} />
+          <Page pageNumber={127} />
+          <Page pageNumber={128} /> */}
+          {/* <Outline
+            onItemClick={o => console.log(o)}
+            onParseSuccess={o => console.info(o)}
+          /> */}
+        </Document>
+      </div>
     );
+  }
 }
 
 export default Book;
